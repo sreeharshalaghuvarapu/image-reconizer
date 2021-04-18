ce07c30cbf3bf4d2b850979e54f4dd4280ee416f
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2021-04-18 10:45:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,12 +14,34 @@ function take_snapshot()
 
 {
      Webcam.snap(function(data_uri){
-         document.getElementById("result").innerHTML = '<img id="captured_image src="'+data_uri+'"/>';
+         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
      });
 }
 
 
 console.log('ml5 version', ml5.version);
+
 classifer=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/TNMB_4Ze1/model.json',modelLoded);
 
+function modelLoded() {
+    console.log('modelLoded!')
+}
+
+function check()
+
+{
+    img = document.getElementById('caputured_image');
+    classifer.classify(img, gotresult);
+}
+
+function gotresult(error, result) {
+    if (error){
+        console.error(error);
+    } else {
+        console.log(result);
+    }
+
+    document.getElementById("result_object_name").innerHTML = result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = result[0].cofidence.toFixed(3);
+}
 
